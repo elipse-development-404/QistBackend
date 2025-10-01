d7da9516ed4408de94f31da67eb6b982d2a5ba98
IamAhmedZaki <mahmedzaki670@gmail.com>
2025-10-01 20:59:34 +0500

    updated

--- src/app.js
+++ src/app.js
@@ -12,6 +12,17 @@ const productDealRoutes = require('./routes/productDealRoutes');
 const topCategoryRoutes = require('./routes/topCategoryRoutes');
 const authCustomer = require('./routes/authCustomer');
 const faqsRoutes = require('./routes/faqsRoutes');
+const aboutRoutes = require('./routes/aboutRoutes');
+const termsAndPrivacyRoutes = require('./routes/termsAndPrivacyRoutes');
+const returnsRefundsPolicyRoutes = require('./routes/returnsRefundsPolicyRoutes');
+const verificationProcessRoutes = require('./routes/verificationProcessRoutes');
+const deliveryPolicyRoutes = require('./routes/deliveryPolicyRoutes');
+const visitUsRoutes = require('./routes/visitUsRoutes');
+const qaRoutes = require('./routes/qaRoutes');
+const agreementRoutes = require('./routes/agreementRoutes');
+const customerAdminRoutes = require('./routes/customerAdminRoutes');
+const organizationSettingsRoutes = require('./routes/organizationSettingsRoutes');
+const contactRoutes = require('./routes/contactRoutes');
 require('dotenv').config();
 
 const app = express();
@@ -21,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.status(200).json({ status: 'success', message: 'Server is running' });
+  res.status(200).json({ status: 'success', message: 'Server is running!!' });
 });
 
 app.use('/api', authRoutes);
@@ -36,6 +47,17 @@ app.use('/api', bannerRoutes);
 app.use('/api', topCategoryRoutes);
 app.use('/api', authCustomer);
 app.use('/api', faqsRoutes);
+app.use('/api', aboutRoutes);
+app.use('/api', termsAndPrivacyRoutes);
+app.use('/api', returnsRefundsPolicyRoutes);
+app.use('/api', verificationProcessRoutes);
+app.use('/api', deliveryPolicyRoutes);
+app.use('/api', visitUsRoutes);
+app.use('/api', qaRoutes);
+app.use('/api', agreementRoutes);
+app.use('/api', customerAdminRoutes);
+app.use('/api', organizationSettingsRoutes);
+app.use('/api', contactRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
